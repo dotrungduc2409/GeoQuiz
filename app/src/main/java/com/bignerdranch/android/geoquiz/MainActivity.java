a2100c318a1d3d45dd7375d44e334b5d77dc681e
DucDo <dotrungduc2409@gmail.com>
2021-05-29 16:52:25 +0700

    Image Button

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.java
@@ -3,6 +3,7 @@ package com.bignerdranch.android.geoquiz;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,7 +13,8 @@ public class MainActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
-    private Button mNextButton;
+    private ImageButton mPreviousImageButton;
+    private ImageButton mNextImageButton;
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[] {
@@ -67,8 +69,19 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        mNextButton = (Button) findViewById(R.id.next_button);
-        mNextButton.setOnClickListener(new View.OnClickListener() {
+        mPreviousImageButton = (ImageButton) findViewById(R.id.previous_button);
+        mPreviousImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+        updateQuestion();
+
+
+        mNextImageButton = (ImageButton) findViewById(R.id.next_button);
+        mNextImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
